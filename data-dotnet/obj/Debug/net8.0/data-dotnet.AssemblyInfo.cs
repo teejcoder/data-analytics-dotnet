33be2cf31447d98040ad4d0fff582dfbe99ba607
teejcoder <96551689+teejcoder@users.noreply.github.com>
2024-08-11 21:11:00 +1000

    symbol change successfully renders symbol's data

--- data-dotnet/obj/Debug/net8.0/data-dotnet.AssemblyInfo.cs
+++ data-dotnet/obj/Debug/net8.0/data-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("data-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5d1f3ac6341017cabe689550c062a12a1f9155")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9a0427d386e7b07d345a74fafbbdab02870be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("data-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("data-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
